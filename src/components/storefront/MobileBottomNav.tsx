d77beaf51c66efa3256f8424e2a3856d57e79859
Lojas <lojasnecessario@demo.com>
2026-08-05 15:45:56 -0300

    fix: remove unused User import

--- src/components/storefront/MobileBottomNav.tsx
+++ src/components/storefront/MobileBottomNav.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { LayoutGrid, Search, Tag, User, MessageCircle } from 'lucide-react';
+import { LayoutGrid, Search, Tag, MessageCircle } from 'lucide-react';
 
 export const MobileBottomNav: React.FC = () => {
   const location = useLocation();
